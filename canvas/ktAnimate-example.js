b168b64bc9421a1970f5494ea022731273d6238a
Mathieu Bruyen <code@mais-h.eu>
2013-01-08 21:51:39 +0100

    Create skeleton code for the hands on parts

---
+++ canvas/ktAnimate-example.js
@@ -0,0 +1,11 @@
+/**
+ * Function that animate the canvas between two frames.
+ *
+ * @param canvas 2D context of the canvas element.
+ * @param fromFrame the frame at the beginning of the animation.
+ * @param toFrame the frame at the end of the animation.
+ * @param callback callback to call with no argument when animation is over.
+ **/
+window.ktAnimate = function(canvas, fromFrame, toFrame, callback) {
+  // TODO
+};
---
+++ canvas/ktDraw.js
@@ -0,0 +1,9 @@
+/**
+ * Function that draws corresponding data into the given canvas.
+ *
+ * @param canvas 2D context of the canvas element.
+ * @param data data to draw.
+ **/
+window.ktDraw = function(canvas, data) {
+  // TODO
+};
