26a5d4a9e8cbafbbdbdc1f238603669dcefe9601
Mathieu Bruyen <code@mais-h.eu>
2013-01-23 12:47:49 +0100

    hands on part for background

---
+++ canvas/ktDrawbackground-example.js
@@ -0,0 +1,19 @@
+/**
+ * Function that draws the background.
+ *
+ * @param canvas the canvas to draw to.
+ * @param size the size of the grid.
+ **/
+window.ktDrawBackground = (function() {
+  return function(canvas, grid) {
+    var ctx = canvas.getContext('2d');
+    for (var i = 0; i < grid; i++) {
+      for (var j = 0; j < grid; j++) {
+        ctx.beginPath();
+        ctx.fillStyle = ((i + j) % 2 === 0) ? 'rgba(30, 130, 30, 0.8)' : 'rgba(30, 30, 230, 0.8)';
+        ctx.rect(i * canvas.width / grid, j * canvas.height / grid, canvas.width / grid, canvas.height / grid);
+        ctx.fill();
+      }
+    }
+  };
+})();
