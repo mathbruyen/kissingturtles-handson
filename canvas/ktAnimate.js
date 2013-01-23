74f34aede47a1282143865bf0706c5ec84b2dd25
Mathieu Bruyen <code@mais-h.eu>
2013-01-23 18:51:22 +0100

    started animation hands on part

---
+++ canvas/ktAnimate.js
@@ -0,0 +1,13 @@
+/**
+ * Function that animate towards a frame.
+ *
+ * @param canvas the canvas to draw to.
+ * @param grid the size of the grid.
+ * @param objects the objects in the new frame.
+ * @param callback the callback to call on animation end.
+ **/
+window.ktAnimate = (function() {
+  return function(canvas, grid, objects, callback) {
+    //TODO
+  };
+})();
