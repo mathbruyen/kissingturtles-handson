7923acdf76144e659ae38defd439f3f0db65f3c5
Mathieu Bruyen <code@mais-h.eu>
2013-01-23 13:37:49 +0100

    fix parameter name in api doc

--- canvas/ktDrawbackground-example.js
+++ canvas/ktDrawbackground-example.js
@@ -2,7 +2,7 @@
  * Function that draws the background.
  *
  * @param canvas the canvas to draw to.
- * @param size the size of the grid.
+ * @param grid the size of the grid.
  **/
 window.ktDrawBackground = (function() {
   return function(canvas, grid) {
